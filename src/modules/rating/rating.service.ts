a07e89727f8ef229530a15205f1b0dd5ecf331f3
priteshtatvasoft <pritesh.makasana@tatvasoft.com>
2025-07-30 14:11:27 +0530

    Add payment refund and realease to provider base on outcome

--- src/modules/rating/rating.service.ts
+++ src/modules/rating/rating.service.ts
@@ -356,6 +356,7 @@ export class RatingService {
     if (['provider_won', 'user_won'].includes(complaint.outcome)) {
       throw new BadRequestException('Complaint is already resolved.');
     }
+
     await this.prismaService.complaint.update({
       where: { id: complaintId },
       data: {
@@ -363,6 +364,12 @@ export class RatingService {
         outcome: outcome,
       },
     });
+
+    if (outcome === "user_won") {
+      await this.stripeService.refundPayment({ gigId: complaint.gig_id });
+    } else {
+      await this.stripeService.realeasePayment({ gigId: complaint.gig_id });
+    };
 
     return {
       success: true,
